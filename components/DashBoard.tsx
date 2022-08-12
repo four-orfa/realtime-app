0123d357ce2855004f7ccc1ed452d0524d2621bd
Toshinori Sumida <alias.ceuta@gmail.com>
2022-08-13 04:44:40 +0900

    Auth

---
+++ components/DashBoard.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import { LogoutIcon, ExclamationCircleIcon } from '@heroicons/react/solid'
+import { supabase } from '../utils/supabase'
+
+const DashBoard: FC = () => {
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+  return (
+    <div>
+      <LogoutIcon
+        className="my-6 h-6 w-6 cursor-pointer text-blue-500"
+        onClick={signOut}
+      />
+    </div>
+  )
+}
+
+export default DashBoard
